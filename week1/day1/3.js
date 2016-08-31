65432844c5b300155e7ba72d8333b2a8714bf819
Jophy Johnson <johnson.jophy27@gmail.com>
2016-08-31 00:38:24 -0500

    number 3 done

--- week1/day1/3.js
+++ week1/day1/3.js
@@ -1,5 +1,28 @@
 var console = {log: args => args}
 
 module.exports = function chessboard() {
-  // write your code here
+var a = '#';
+var b = ' ';
+var c = '';
+var counter = 0; 
+var size = 8;
+var gridSize = size * (size + 1); 
+
+
+console.log(gridSize) 
+
+for(var x = 0; x < gridSize; x++) {
+	counter++;
+	if( x % 2 !== 0 && counter !== (size + 1)) {
+		c = c + b; 
+	} else if ( x % 2 === 0 && counter !== (size + 1)) {
+		c = c + a;
+	} else if ( x === (gridSize - 1)) {
+		break;
+	} else {
+		c = c + '\n'
+		counter = 0;
+	}
+}
+console.log(c)
 }
