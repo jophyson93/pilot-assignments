26c5bc327617ac0cd9c3e4ac4d82c79309e219d0
Jophy Johnson <johnson.jophy27@gmail.com>
2016-08-31 11:32:15 -0500

    another solution for number 3 added

--- week1/day1/3.js
+++ week1/day1/3.js
@@ -21,5 +21,34 @@ for( var x = 0; x < size; x++) {
 		c = c + '\n';
 	}
 }
+
+
+
+ // or 
+
+/* 
+var a = '# ';
+var b = ' #';
+var c = ''; 
+var size =8;
+for( var x = 0; x < size; x++) {
+	for( var y = 0; y < (size / 2); y++) {
+		if(x % 2 === 0) {
+			c = c + a; 
+		} else {
+			c = c + b;
+		}
+	}
+	if( x !== (size - 1)) {
+		c = c + '\n';
+	}
+}
+console.log(c)
+
+*/
+
+
+
+
 console.log(c)
 }
