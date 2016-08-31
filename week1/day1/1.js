cae50493f7a0f053f1e13badff1fde31c0f9ff7e
Jophy Johnson <johnson.jophy27@gmail.com>
2016-08-31 00:39:56 -0500

    number 2 and 1 done

--- week1/day1/1.js
+++ week1/day1/1.js
@@ -1,5 +1,10 @@
 var console = {log: args => args}
 
 module.exports = function triangle() {
-  
+var z = y = '#';
+
+for(var x = 0; x < 7; x++) {
+	console.log(z)
+	z = z + y;
+}
 }
--- week1/day1/2.js
+++ week1/day1/2.js
@@ -1,5 +1,20 @@
 var console = {log: args => args}
 
 module.exports = function fizzBuzz() {
-  // write your code here
+var fizz = 'fizz'
+var buzz = 'buzz'
+var fizzbuzz = 'fizzbuzz'
+
+for (x = 1; x < 101; x++) {
+	if(x % 3 === 0 && x % 5 !== 0) {
+		console.log(fizz)
+	} else if (x % 5 === 0 && x % 3 !== 0) {
+		console.log(buzz)
+	} 
+	else if (x % 3 === 0 && x % 5 === 0) {
+		console.log(fizzbuzz)
+	} else {
+		console.log(x)
+	}
+}
 }
