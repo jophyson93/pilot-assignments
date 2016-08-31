1a617d396b5bbc28f3bcdbd054220d9da5312f3f
Jophy Johnson <johnson.jophy27@gmail.com>
2016-08-31 09:20:50 -0500

    number 3 redone with better solution

--- week1/day1/3.js
+++ week1/day1/3.js
@@ -1,27 +1,24 @@
 var console = {log: args => args}
 
 module.exports = function chessboard() {
-var a = '#';
-var b = ' ';
-var c = '';
-var counter = 0; 
-var size = 8;
-var gridSize = size * (size + 1); 
-
-
-console.log(gridSize) 
-
-for(var x = 0; x < gridSize; x++) {
-	counter++;
-	if( x % 2 !== 0 && counter !== (size + 1)) {
-		c = c + b; 
-	} else if ( x % 2 === 0 && counter !== (size + 1)) {
-		c = c + a;
-	} else if ( x === (gridSize - 1)) {
-		break;
+var a = '# ';
+var b = ' #';
+var c = ''; 
+var d = '';
+var counter = 1;
+var size =8;
+for( var x = 0; x < size; x++) {
+	if( x % 2 === 0) {
+		for( var y = 0; y < (size / 2); y++) {
+			c = c + a;
+		}
 	} else {
-		c = c + '\n'
-		counter = 0;
+		for( var y = 0; y < (size / 2); y++) {
+			c = c + b;
+		}
+	}
+	if( x !== (size-1)) {
+		c = c + '\n';
 	}
 }
 console.log(c)
